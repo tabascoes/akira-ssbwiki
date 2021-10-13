1cfc251dc5e69619bdb32cbaf9545fdd5b32244c
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-10-13 20:07:44 +0900

    Change pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import 'nextra-theme-docs/style.css'
 
 export default function Nextra({ Component, pageProps }) {
+  useEffect(() => {
+	  // ここに全ページ共通で行う処理
+    return (<div>a</div>);
+  },[router.pathname])
   return <Component {...pageProps} />
 }
