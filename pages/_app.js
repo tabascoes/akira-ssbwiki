f23a52bde74733b29490252be92494053093110a
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-10-13 20:17:06 +0900

    reveal `_app.js`

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,5 @@ import 'nextra-theme-docs/style.css'
 import { useEffect } from 'react';
 
 export default function Nextra({ Component, pageProps }) {
-  useEffect(() => {
-	  // ここに全ページ共通で行う処理
-    return (<div>a</div>);
-  },[router.pathname])
   return <Component {...pageProps} />
 }
