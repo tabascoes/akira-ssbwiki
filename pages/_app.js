e2acb761f5cf2cc1b838019fbdd1a4015d4b1a23
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-10-13 21:46:41 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import 'nextra-theme-docs/style.css'
-import '../fonts.css'
+import './fonts.css'
 
 export default function Nextra({ Component, pageProps }) {
   return <Component {...pageProps} />
