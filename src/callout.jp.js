1f8f9203741718907cacd19fba221fc5853009ce
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-10-14 20:42:35 +0900

    Change pyra-mythra pages

---
+++ src/callout.jp.js
@@ -0,0 +1,7 @@
+import Callout from 'nextra-theme-docs/callout'
+
+export function charactorsinone(name1, path1, name2, path2) {
+    '<Callout emoji="👥">'
+      'このキャラクターは2つあり、互いに特徴や技が異なっているので、[' + name1 + '](' + path1 + ')と[' + name2 + '](' + path2 + ')に分けて紹介しています。'
+    '</Callout>'
+  }
