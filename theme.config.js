01887424fafca688be9f38a5d4c930ec630ac7cb
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-10-14 22:10:39 +0900

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,10 +1,10 @@
 export default {
-  repository: 'https://github.com/tabascoes/akira-ssbwiki',
-  docsRepository: 'https://github.com/tabascoes/akira-ssbwiki',
-  titleSuffix: ' – SSBWiki',
+  repository: 'https://github.com/tabascoes/next-ssbwiki',
+  docsRepository: 'https://github.com/tabascoes/next-ssbwiki',
+  titleSuffix: ' – SSBWikiNext',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">UtamakeWikiPortal</span>
+      <span className="mr-2 font-extrabold hidden md:inline">SSBWikiNext</span>
       <span className="text-gray-600 font-normal hidden md:inline">
         For beginner to senior
       </span>
