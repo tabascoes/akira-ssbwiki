b51ca9e8bb5406eb9a9bf705ec2d0ce5a5103a9d
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-10-15 23:34:39 +0900

    Update callout.jp.js

--- src/callout.jp.js
+++ src/callout.jp.js
@@ -1,6 +1,6 @@
 import Callout from 'nextra-theme-docs/callout'
 
-exports charactorsinone(name1, path1, name2, path2) {
+export function charactorsinone(name1, path1, name2, path2) {
     '<Callout emoji="👥">'
       'このキャラクターは2つあり、互いに特徴や技が異なっているので、[' + name1 + '](' + path1 + ')と[' + name2 + '](' + path2 + ')に分けて紹介しています。'
     '</Callout>'
